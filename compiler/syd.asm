0ef55083a8ac6b8eac93b93fed9007eeff968ef4
treeplate <tree.plate.moo@gmail.com>
2022-02-13 16:02:15 -0800

    return type checking

--- compiler/syd.asm
+++ compiler/syd.asm
@@ -20,16 +20,19 @@ extern ExitProcess : proc
    ; 0 0   Boolean'3
    ; 0 0   NullFunction(Anything...)'4
    ; 0 0   NullFunction(Integer)'5
-   ; 0 0   IntegerFunction()'6
+   ; 0 0   NullFunction()'6
    ; 1 0   Integer'7
    ; 0 1   String'8
 
   parameterCountCheckFailureMessage dq -01h                        ; String constant (reference count)
                dq 88                                               ; Length
-               db "error: function call received the wrong number of parameters (expected %d, received %d)", 0ah ; line 1215 column 25 in file syd-compiler.syd
+               db "error: function call received the wrong number of parameters (expected %d, received %d)", 0ah ; line 1223 column 25 in file syd-compiler.syd
   parameterTypeCheckFailureMessage dq -01h                         ; String constant (reference count)
                dq 71                                               ; Length
-               db "error: type mismatch for function %s parameter %d, expected %s, got %s", 0ah ; line 1220 column 25 in file syd-compiler.syd
+               db "error: type mismatch for function %s parameter %d, expected %s, got %s", 0ah ; line 1228 column 25 in file syd-compiler.syd
+  returnValueTypeCheckFailureMessage dq -01h                       ; String constant (reference count)
+               dq 68                                               ; Length
+               db "error: type mismatch for function return value, expected %s, got %s", 0ah ; line 1233 column 25 in file syd-compiler.syd
 
 .code
 
@@ -47,29 +50,31 @@ main:
   ; ========
   ; Prolog
   push rbp                                                         ; save volatile registers
-  sub rsp, 078h                                                    ; allocate space for stack
-  lea rbp, [rsp+078h]                                              ; set up frame pointer
-  ; Line 1: Integer three() { ...
-  ; Line 5: exit(three());
+  sub rsp, 050h                                                    ; allocate space for stack
+  lea rbp, [rsp+050h]                                              ; set up frame pointer
+  ; Prepare local variables for function call
   mov qword ptr [rbp-008h], 0h                                     ; value of this pointer
   mov qword ptr [rbp-010h], 000000000h                             ; type of this pointer
   mov qword ptr [rbp-018h], 0h                                     ; value of closure pointer
-  ; Calling func$three with 0 arguments
+  ; Calling func$print with 1 arguments
+  push func$foo                                                    ; value of argument #1
+  push 000000006h                                                  ; type of argument #1
   lea r11, [rbp-020h]                                              ; pointer to return value (and type, 8 bytes earlier)
   push r11                                                         ; (that pointer is the last value pushed to the stack)
   lea r9, [rbp-008h]                                               ; pointer to this
   mov r8, [rbp-010h]                                               ; type of this
   lea rdx, [rbp-018h]                                              ; pointer to closure
-  mov rcx, 0                                                       ; number of arguments
+  mov rcx, 1                                                       ; number of arguments
   sub rsp, 20h                                                     ; allocate shadow space
-  call func$three                                                  ; jump to subroutine
-  add rsp, 028h                                                    ; release shadow space and arguments
+  call func$print                                                  ; jump to subroutine
+  add rsp, 038h                                                    ; release shadow space and arguments
+  ; Prepare local variables for function call
   mov qword ptr [rbp-030h], 0h                                     ; value of this pointer
   mov qword ptr [rbp-038h], 000000000h                             ; type of this pointer
   mov qword ptr [rbp-040h], 0h                                     ; value of closure pointer
   ; Calling func$exit with 1 arguments
-  push [rbp-020h]                                                  ; value of argument #1
-  push [rbp-028h]                                                  ; type of argument #1
+  push 000000000h                                                  ; value of argument #1
+  push 000000007h                                                  ; type of argument #1
   lea r11, [rbp-048h]                                              ; pointer to return value (and type, 8 bytes earlier)
   push r11                                                         ; (that pointer is the last value pushed to the stack)
   lea r9, [rbp-030h]                                               ; pointer to this
@@ -79,23 +84,8 @@ main:
   sub rsp, 20h                                                     ; allocate shadow space
   call func$exit                                                   ; jump to subroutine
   add rsp, 038h                                                    ; release shadow space and arguments
-  mov qword ptr [rbp-058h], 0h                                     ; value of this pointer
-  mov qword ptr [rbp-060h], 000000000h                             ; type of this pointer
-  mov qword ptr [rbp-068h], 0h                                     ; value of closure pointer
-  ; Calling func$exit with 1 arguments
-  push 000000000h                                                  ; value of argument #1
-  push 000000007h                                                  ; type of argument #1
-  lea r11, [rbp-070h]                                              ; pointer to return value (and type, 8 bytes earlier)
-  push r11                                                         ; (that pointer is the last value pushed to the stack)
-  lea r9, [rbp-058h]                                               ; pointer to this
-  mov r8, [rbp-060h]                                               ; type of this
-  lea rdx, [rbp-068h]                                              ; pointer to closure
-  mov rcx, 1                                                       ; number of arguments
-  sub rsp, 20h                                                     ; allocate shadow space
-  call func$exit                                                   ; jump to subroutine
-  add rsp, 038h                                                    ; release shadow space and arguments
   ; Epilog
-  add rsp, 078h                                                    ; free space for stack
+  add rsp, 050h                                                    ; free space for stack
   pop rbp                                                          ; restore volatile registers
   ret                                                              ; exit application
 
@@ -109,7 +99,8 @@ func$print:
   mov r15, [rbp+038h]                                              ; prepare return value
   ; Check parameter count
   cmp rcx, 000000001h                                              ; check number of parameters is as expected
-  je func$print$paramCountGood                                     ; skip next block if they are equal
+  je func$print$parameterCount$Ok                                  ; skip next block if they are equal
+    ; Prepare local variables for function call
     mov qword ptr [rbp-010h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-018h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-020h], 0h                                   ; value of closure pointer
@@ -126,6 +117,7 @@ func$print:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$print                                                ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
+    ; Prepare local variables for function call
     mov qword ptr [rbp-038h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-040h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-048h], 0h                                   ; value of closure pointer
@@ -141,13 +133,14 @@ func$print:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$exit                                                 ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
-  func$print$paramCountGood:
+  func$print$parameterCount$Ok:
   ; Check type of parameter 0, message to print to console (expecting String)
   mov rax, [rbp+040h]                                              ; load the dynamic type of message to print to console into rax
   lea r10, typeTable                                               ; move type table offset into r10
   add rax, r10                                                     ; adjust rax to point to the type table
   bt qword ptr [rax], 1                                            ; check that message to print to console is String'8
-  jc func$print$param1$TypeGood                                    ; skip next block if the type matches
+  jc func$print$messageToPrintToConsole$Ok                         ; skip next block if the type matches
+    ; Prepare local variables for function call
     mov qword ptr [rbp-060h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-068h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-070h], 0h                                   ; value of closure pointer
@@ -164,6 +157,7 @@ func$print:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$print                                                ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
+    ; Prepare local variables for function call
     mov qword ptr [rbp-088h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-090h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-098h], 0h                                   ; value of closure pointer
@@ -179,7 +173,7 @@ func$print:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$exit                                                 ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
-  func$print$param1$TypeGood:
+  func$print$messageToPrintToConsole$Ok:
   ; Calling GetStdHandle
   mov rcx, -11                                                     ; argument #1: STD_OUTPUT_HANDLE
   call GetStdHandle                                                ; handle returned in rax
@@ -211,7 +205,8 @@ func$exit:
   mov r15, [rbp+038h]                                              ; prepare return value
   ; Check parameter count
   cmp rcx, 000000001h                                              ; check number of parameters is as expected
-  je func$exit$paramCountGood                                      ; skip next block if they are equal
+  je func$exit$parameterCount$Ok                                   ; skip next block if they are equal
+    ; Prepare local variables for function call
     mov qword ptr [rbp-008h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-010h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-018h], 0h                                   ; value of closure pointer
@@ -228,6 +223,7 @@ func$exit:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$print                                                ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
+    ; Prepare local variables for function call
     mov qword ptr [rbp-030h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-038h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-040h], 0h                                   ; value of closure pointer
@@ -243,13 +239,14 @@ func$exit:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$exit                                                 ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
-  func$exit$paramCountGood:
+  func$exit$parameterCount$Ok:
   ; Check type of parameter 0, exit code parameter (expecting Integer)
   mov rax, [rbp+040h]                                              ; load the dynamic type of exit code parameter into rax
   lea r10, typeTable                                               ; move type table offset into r10
   add rax, r10                                                     ; adjust rax to point to the type table
   bt qword ptr [rax], 0                                            ; check that exit code parameter is Integer'7
-  jc func$exit$param1$TypeGood                                     ; skip next block if the type matches
+  jc func$exit$exitCodeParameter$Ok                                ; skip next block if the type matches
+    ; Prepare local variables for function call
     mov qword ptr [rbp-058h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-060h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-068h], 0h                                   ; value of closure pointer
@@ -266,6 +263,7 @@ func$exit:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$print                                                ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
+    ; Prepare local variables for function call
     mov qword ptr [rbp-080h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-088h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-090h], 0h                                   ; value of closure pointer
@@ -281,7 +279,7 @@ func$exit:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$exit                                                 ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
-  func$exit$param1$TypeGood:
+  func$exit$exitCodeParameter$Ok:
   ; Calling ExitProcess
   mov rcx, [rbp+048h]                                              ; exit code
   sub rsp, 20h                                                     ; allocate shadow space
@@ -295,8 +293,8 @@ func$exit:
   pop rbp                                                          ; restore volatile registers
   ret                                                              ; return from subroutine
 
-; three
-func$three:
+; foo
+func$foo:
   ; Prolog
   push rbp                                                         ; save volatile registers
   push r15                                                         ; save volatile registers
@@ -305,7 +303,8 @@ func$three:
   mov r15, [rbp+038h]                                              ; prepare return value
   ; Check parameter count
   cmp rcx, 000000000h                                              ; check number of parameters is as expected
-  je func$three$paramCountGood                                     ; skip next block if they are equal
+  je func$foo$parameterCount$Ok                                    ; skip next block if they are equal
+    ; Prepare local variables for function call
     mov qword ptr [rbp-008h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-010h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-018h], 0h                                   ; value of closure pointer
@@ -322,6 +321,7 @@ func$three:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$print                                                ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
+    ; Prepare local variables for function call
     mov qword ptr [rbp-030h], 0h                                   ; value of this pointer
     mov qword ptr [rbp-038h], 000000000h                           ; type of this pointer
     mov qword ptr [rbp-040h], 0h                                   ; value of closure pointer
@@ -337,12 +337,8 @@ func$three:
     sub rsp, 20h                                                   ; allocate shadow space
     call func$exit                                                 ; jump to subroutine
     add rsp, 038h                                                  ; release shadow space and arguments
-  func$three$paramCountGood:
-  ; Line 2: return 3 /* 0x3 */;
-  mov qword ptr [r15], 000000003h                                  ; value of return value
-  mov qword ptr [r15-08h], 000000007h                              ; type of return value
-  jmp func$three$epilog                                            ; return
-  func$three$epilog:
+  func$foo$parameterCount$Ok:
+  func$foo$epilog:
   mov rax, r15                                                     ; report address of return value
   ; Epilog
   add rsp, 050h                                                    ; free space for stack
